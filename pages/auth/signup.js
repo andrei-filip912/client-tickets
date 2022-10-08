9df80681731358bd3426850656806637070bfe37
andrei-filip912 <filip.andrei912@gmail.com>
2022-10-08 18:53:26 +0200

    updated signup page ui

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Router from 'next/router';
+import Image from 'next/image';
 import useRequest  from '../../hooks/use-request';
+import draw2 from '../../public/draw2.svg';
 
 const Signup = () => {
     const [email, setEmail] = useState('');
@@ -20,26 +22,47 @@ const Signup = () => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <h1>Sign up!</h1>
-            <div className='margin-bottom'>
-                <label>Email Address</label>
-                <input 
-                    value={email} 
-                    onChange={e => setEmail(e.target.value)} 
-                    className='form-control' 
-                />
+        <section className="vh-100">
+            <div className="container py-5 h-100">
+                <div className="row d-flex align-items-center justify-content-center h-100">
+                    <div className="col-md-8 col-lg-7 col-xl-6">
+                        <div className="img-fluid">
+                            <Image src={draw2}
+                                alt="Phone image">
+                            </Image>
+                        </div>
+                    </div>
+                    <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                        <h1 className='margin-bottom'>Sign up</h1>
+                        <form onSubmit={onSubmit}>
+
+                            <div className="form-outline mb-4">
+                                <label className="form-label">Email address</label>
+                                <input
+                                    type="email"
+                                    className="form-control form-control-lg"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="form-outline mb-4">
+                                <label className="form-label">Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control form-control-lg"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                            </div>
+                            
+                            {errors}
+                            <button type="submit" className="btn btn-primary btn-lg btn-block">Sign in</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div className='margin-bottom'>
-                <label>Password Address</label>
-                <input 
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    type='password' className='form-control' />
-            </div>
-            {errors}
-            <button className='btn btn-primary'>Signup</button>
-        </form>
+        </section>
     )
 }
 
