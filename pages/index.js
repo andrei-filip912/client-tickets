da20d602004f6c847dbc843cbf8aac39fd989f6c
andrei-filip912 <filip.andrei912@gmail.com>
2022-10-10 21:08:52 +0200

    Update index.js
    redirect on buy ticket

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import bg from '../public/bg.jpg';
-import ticket from '../public/ticket.jpg';
-
+// import ticket from '../public/ticket.jpg';
+import { useRouter } from 'next/router';
 
 const Index = ({ currentUser, tickets }) => {
+    const router = useRouter();
+
     const ticketList = tickets.map(ticket => {
         return (
             <div key={ticket.id} className="col">
@@ -12,7 +14,7 @@ const Index = ({ currentUser, tickets }) => {
                     <div className="card-body">
                         <h5 className="card-title">{ticket.title}</h5>
                         <p className="card-text">Price: {ticket.price}.</p>
-                        <a href="#" className="btn btn-primary">Buy</a>
+                        <a onClick={() => router.push('/tickets/[ticketId]',`/tickets/${ticket.id}`)} className="btn btn-primary">Buy</a>
                     </div>
                 </div>
             </div>
