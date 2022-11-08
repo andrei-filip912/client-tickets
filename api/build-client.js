e38343df4af58f256e7ef4002874f96e911c052e
andrei-filip912 <filip.andrei912@gmail.com>
2022-11-08 16:33:06 +0100

    orders index page styling
    changed base url for dev

--- api/build-client.js
+++ api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     if (typeof window === 'undefined') {
         // request will be sent from server
         return axios.create({
-            baseURL: 'http://www.tickets-buy-now.shop',
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
             headers: req.headers
         });
 
